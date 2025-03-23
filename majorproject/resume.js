33769e7f8abe381616f8c1af4dfc12320671e65b
shree8599 <shreemandhare5087@gmail.com>
2025-03-23 19:09:18 +0530

    styling of resume

--- majorproject/resume.js
+++ majorproject/resume.js
@@ -5,6 +5,21 @@ const image = document.querySelector(".image-container");
 const submit = document.querySelector(".submit");
 const resume = document.querySelector(".resume");
 const body = document.querySelector("body");
+
+const about=document.querySelectorAll(".about[input]");
+const photo = document.querySelector(".photo");
+const fn = document.querySelector("#fn");
+const mn = document.querySelector("#mn");
+const ln = document.querySelector("#ln");
+const designation = document.querySelector("#designation");
+const email = document.querySelector("#email");
+const address = document.querySelector("#address");
+const phone = document.querySelector("#phone");
+const summary = document.querySelector("#summary");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const skills = document.querySelector("#skills");
+
 button.addEventListener('click', () => {
     signin.style.display = "flex";
     close.style.display = "none";
@@ -15,7 +30,13 @@ submit.addEventListener('click', (e) => {
     e.preventDefault();
     signin.style.display = "none";
     resume.style.display = "flex";
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
 
+});
+
+about.addEventListener('input', (e) => {
+    
 });
 
 
